cf99562a58ea616bb594cfb9e9b06b869cce79bd
Anwesh43 <anweshthecool0@gmail.com>
2017-01-03 23:35:44 +0530

    Adding socketiomodule

--- socketiomodule/src/main/java/com/anwesome/ui/socketiomodule/SocketIOUtil.java
+++ socketiomodule/src/main/java/com/anwesome/ui/socketiomodule/SocketIOUtil.java
@@ -1,7 +1,16 @@
 package com.anwesome.ui.socketiomodule;
 
-/**
- * Created by anweshmishra on 03/01/17.
- */
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
 public class SocketIOUtil {
+    public static Socket getIOSocket(String address) {
+        try {
+            Socket socket = IO.socket(address);
+            return socket.connect();
+        }
+        catch(Exception ex) {
+
+        }
+        return null;
+    }
 }
